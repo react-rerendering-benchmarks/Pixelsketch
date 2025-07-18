1ce6718f856e814320cb0245365e01c6a2d919ac
Alexi Turcotte <alexi@MAC.local>
2025-07-18 22:12:57 +0200

    added transformation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-
-import Grid from './components/Grid.tsx'
-
-function App() {
-
-
-  return (
-    <div className='app'>
+import { memo } from "react";
+import Grid from './components/Grid.tsx';
+const App = memo(function App() {
+  return <div className='app'>
       <h1>Pixelsketch</h1>
       <Grid></Grid>
-    </div>
-  )
-}
-
-export default App
+    </div>;
+});
+export default App;
