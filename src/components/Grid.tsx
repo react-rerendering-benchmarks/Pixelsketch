b2a2b2c1b7d5755f27e454730227c23f9b0f4ab6
Alexi Turcotte <alexi@MAC.local>
2025-07-18 22:10:48 +0200

    med config

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,7 +3,7 @@ import { Pixel } from './Pixel.tsx';
 import Toolbar from "./Toolbar.tsx";
 
 function Grid() {
-  const DEFAULT_SIZE = 225;
+  const DEFAULT_SIZE = 64 * 64;
   const DEFAULT_BACKGROUND_COLOR = 'white';
   const DEFAULT_COLOR = '#F58616';
 
